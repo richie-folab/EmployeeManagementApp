953ab7ec8eac5343598e72e6f0e227063dd8cba6
richie-folab <richie_folab@yahoo.com>
2022-01-26 21:04:12 +0100

    Created EmployeeRepository interface

---
+++ employee-management-api/src/main/java/com/richardella/employeemanagement/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.richardella.employeemanagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.richardella.employeemanagement.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
