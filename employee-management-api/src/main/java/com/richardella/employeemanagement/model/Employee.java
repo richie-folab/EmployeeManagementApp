9459fe6d1e3dea81dda041879594d95cd7da90a3
richie-folab <richie_folab@yahoo.com>
2022-01-26 20:53:17 +0100

    first commit

---
+++ employee-management-api/src/main/java/com/richardella/employeemanagement/model/Employee.java
@@ -0,0 +1,64 @@
+package com.richardella.employeemanagement.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public long id;
+	
+	@Column(name="first_name")
+	public String firstName;
+	
+	@Column(name="last_name")
+	public String lastName;
+	
+	@Column(name="email_address")
+	public String emailAddress;
+	
+	public Employee() {
+
+	}
+
+	public Employee(String firstName, String lastName, String emailAddress) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailAddress = emailAddress;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	
+	
+
+}
---
+++ employee-management-api/src/test/java/com/richardella/employeemanagement/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richardella.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
