a6e95c6e9cbd37df3523e853166eefde8691f022
richie-folab <richie_folab@yahoo.com>
2022-01-27 21:48:53 +0100

    Created GetAllEmployees method

---
+++ employee-management-api/src/main/java/com/richardella/employeemanagement/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.richardella.employeemanagement.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.richardella.employeemanagement.model.Employee;
+import com.richardella.employeemanagement.repository.EmployeeRepository;
+
+@RestController
+@RequestMapping("/api/employees/")
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeRepository employeeRespository;
+	
+	//Get all employees
+	@GetMapping("/all")
+	public Iterable<Employee> getAllEmployees() {
+		
+		return employeeRespository.findAll();
+	}
+ 	
+
+}
--- employee-management-api/src/main/java/com/richardella/employeemanagement/repository/EmployeeRepository.java
+++ employee-management-api/src/main/java/com/richardella/employeemanagement/repository/EmployeeRepository.java
@@ -1,11 +1,12 @@
 package com.richardella.employeemanagement.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.richardella.employeemanagement.model.Employee;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
 
 }
